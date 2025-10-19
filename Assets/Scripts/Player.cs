fcb02601b19cd743b9e47038b00684af9b4e8659
Tango <maxim.ant07@gmail.com>
2025-10-19 21:50:29 +0300

    Add files via upload

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ using NUnit.Framework.Interfaces;
 
 public class Player : MonoBehaviour
 {
+    // private
+    // -------
     [System.Obsolete]
     private void Start()
     {
@@ -21,29 +23,80 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
+        handleMovement();
+        handleInteractions();
+    }
+
+    // Interactions
+    private void handleInteractions()
+    {
         Vector2 inputVec = mGameInput.getMovementVector(true);
 
         Vector3 moveDir = new Vector3(inputVec.x, 0.0f, inputVec.y);
 
+        if (moveDir != Vector3.zero)
+            mLastInteraction = moveDir;
+
+        float distanceInteraction = 2.0f;
+        bool hasInteraction = Physics.Raycast(transform.position, mLastInteraction, out RaycastHit outRaycastHitInfo, distanceInteraction);
+    }
+
+    // Movement
+    private void handleMovement()
+    {
+        Vector2 inputVec = mGameInput.getMovementVector(true);
+
+        Vector3 moveDir = new Vector3(inputVec.x, 0.0f, inputVec.y);
+
+        bool canMove = handleCollision(ref moveDir);
+        if (canMove)
+            transform.position += moveDir * mMoveSpeed * Time.deltaTime;
+
+        handleRotation(inputVec, moveDir);
+    }
+    private void handleRotation(Vector2 pInputVec, Vector3 pMoveDir)
+    {
+        mIsWalking = pInputVec != Vector2.zero;
+        if (mIsWalking)
+        {
+            float rotateSpeed = 10.0f;
+            Quaternion targetRotation = Quaternion.LookRotation(pMoveDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+        }
+    }
+    private bool handleCollision(ref Vector3 pMoveDir)
+    {
         float moveDistance = mMoveSpeed * Time.deltaTime;
         float playerSize = 0.7f;
         float playerHeight = 2.0f;
 
-        bool canMove = !Physics.CapsuleCast(transform.forward, transform.position + Vector3.up * playerHeight, playerSize, moveDir, moveDistance);
-        if(canMove)
-            transform.position += moveDir * moveDistance;
-
-        mIsWalking = inputVec != Vector2.zero;
-        if (mIsWalking)
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, pMoveDir, moveDistance);
+        if (!canMove)
         {
-            float rotateSpeed = 5.0f;
-            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+            Vector3 moveDirX = new Vector3(pMoveDir.x, 0.0f, 0.0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirX, moveDistance);
+            if (canMove)
+                pMoveDir = moveDirX;
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0.0f, 0.0f, pMoveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirZ, moveDistance);
+                if (canMove)
+                    pMoveDir = moveDirZ;
+            }
         }
+
+        return canMove;
     }
+    // -------
+
+    // public
+    // ------
     public bool isWalking()
     {
         return mIsWalking;
     }
+    // ------
 
     // variables
     // ---------
@@ -51,6 +104,7 @@ public class Player : MonoBehaviour
     private float mMoveSpeed = 5.0f;
     [SerializeField]
     private GameInput mGameInput;
+    private Vector3 mLastInteraction;
 
     private bool mIsWalking;
     // ---------
