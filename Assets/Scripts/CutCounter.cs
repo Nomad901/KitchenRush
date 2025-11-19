41f8c73dc51a4e0eb78048493b30c8d1df5c96ab
Tango <maxim.ant07@gmail.com>
2025-11-19 08:58:04 +0300

    Update CutCounter.cs

--- Assets/Scripts/CutCounter.cs
+++ Assets/Scripts/CutCounter.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class CutCounter : BaseCounter
 {
+    private void Start()
+    {
+        mKitchenObjectNames = new string[(Int32)KitchenObjectType.NUM_OF_OBJECTS] { "Tomato", "Cheese", "Cabbage", "Error" };
+    }
     public override void interact(Player pPlayer)
     {
         if (!hasKitchenObject())
@@ -21,13 +28,69 @@ public class CutCounter : BaseCounter
     {
         if(hasKitchenObject())
         {
+            string kitchenObjectName = manageString(getKitchenObject()?.name);
+            manageKitchenObjectType(kitchenObjectName);
+            if (mKitchenObjectType == KitchenObjectType.ERROR)
+                return;
+
             getKitchenObject().destroySelf();
 
-            KitchenObject.spawnKitchenObject(mKitchenScriptObject, this);
+            UInt32 indexKitchenScriptObjects = 0;
+            switch (mKitchenObjectType)
+            {
+                case KitchenObjectType.TOMATO:
+                    indexKitchenScriptObjects = 0;
+                    break;
+                case KitchenObjectType.CHEESE:
+                    indexKitchenScriptObjects = 1;
+                    break;
+                case KitchenObjectType.CABBAGE:
+                    indexKitchenScriptObjects = 2;
+                    break;
+                default:
+                    break;
+            }
+
+            KitchenObject.spawnKitchenObject(mKitchenScriptObject[indexKitchenScriptObjects], this);
         }
     }
 
-    [SerializeField]
-    private KitchenScriptObject mKitchenScriptObject;
+    //
+    // manages string through clearing redundant braces;
+    // for example: Tomato(clone) -> Tomato;
+    //
+    private string manageString(string pString)
+    {
+        if(pString.Contains('(') && pString.Contains(')'))
+        {
+            Int32 indexOfFirstBrace = pString.IndexOf('(');
+            pString = pString.Remove(indexOfFirstBrace);
+        }
+        return pString;
+    }
+    private void manageKitchenObjectType(string pKitchenObjectName)
+    {
+        if (pKitchenObjectName == mKitchenObjectNames[(Int32)KitchenObjectType.TOMATO])
+            mKitchenObjectType = KitchenObjectType.TOMATO;
+        else if (pKitchenObjectName == mKitchenObjectNames[(Int32)KitchenObjectType.CHEESE])
+            mKitchenObjectType = KitchenObjectType.CHEESE;
+        else if (pKitchenObjectName == mKitchenObjectNames[(Int32)KitchenObjectType.CABBAGE])
+            mKitchenObjectType = KitchenObjectType.CABBAGE;
+        else
+            mKitchenObjectType = KitchenObjectType.ERROR;
+    }
 
+    [SerializeField]
+    private KitchenScriptObject[] mKitchenScriptObject;
+
+    private enum KitchenObjectType
+    {
+        TOMATO         = 0,
+        CHEESE         = 1,
+        CABBAGE        = 2,
+        ERROR          = 3,
+        NUM_OF_OBJECTS = 4
+    };
+    private string[] mKitchenObjectNames;
+    private KitchenObjectType mKitchenObjectType;
 }
