db0ee522ab707c5a1fe6ba132cc0ecd1aa5dbad9
Nomad901 <maxim.ant07@gmail.com>
2025-12-12 11:50:56 +0300

    conveyers

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class DeliveryCounter : BaseCounter
 {
-    
+    public override void interact(Player pPlayer)
+    {
+        if(pPlayer.hasKitchenObject())
+        {
+            if(pPlayer.getKitchenObject().tryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                pPlayer.getKitchenObject().destroySelf();
+            }
+        }
+    }
 }
