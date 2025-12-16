5207d1207779f5573c461e572e3ef9c8729d1031
Nomad901 <maxim.ant07@gmail.com>
2025-12-16 17:01:48 +0300

    DeliveryManager UI final

--- Assets/Scripts/UI/DeliveryManagerUI_Single.cs
+++ Assets/Scripts/UI/DeliveryManagerUI_Single.cs
@@ -1,13 +1,37 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeliveryManagerUI_Single : MonoBehaviour
 {
+    private void Awake()
+    {
+        mIconTemplate.gameObject.SetActive(false);
+    }
     public void setRecipeSOName(RecipeSO pRecipeSO)
     {
         mRecipeNameText.text = pRecipeSO.mRecipeName;
+
+        foreach (Transform child in mIconContainer)
+        {
+            if (child == mIconTemplate)
+                continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenScriptObject kitchenObjSO in pRecipeSO.mNeededKitchenObjects)
+        {
+            Transform iconTransform = Instantiate(mIconTemplate, mIconContainer);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<Image>().sprite = kitchenObjSO.mSprite;
+        }
+
     }
 
     [SerializeField]
     private TextMeshProUGUI mRecipeNameText;
+    [SerializeField]
+    private Transform mIconContainer;
+    [SerializeField]
+    private Transform mIconTemplate;
 }
